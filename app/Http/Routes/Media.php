261a7d12f12d2f4450a6cdd9939b08e58cc74962
Aric Belsito <lluixhi@gmail.com>
2016-09-22 18:34:40 -0700

    Possibly fix routes a bit more.

--- app/Http/Routes/Media.php
+++ app/Http/Routes/Media.php
@@ -16,28 +16,28 @@
  * Media queries for actual content.
  * Can be routed through a CDN (sub)domain.
  */
-if (false && !is_hidden_service() && config('app.url_media', false))
+if (!is_hidden_service() && !config('app.url_media', false))
 {
     Route::group(['domain' => config('app.url_media')], function() {
-        Route::get('{attachment}/{filename}', [
-            'as'   => 'file.attachment',
-            'uses' => 'FileController@getImageFromAttachment',
-        ]);
-
         Route::get('{hash}/{filename}', [
             'as'   => 'file.hash',
             'uses' => 'FileController@getImageFromHash',
-            ])->where(['hash' => "[a-f0-9]{32}",]);
+        ])->where(['hash' => "[a-f0-9]{32}",]);
 
-        Route::get('thumb/{attachment}/{filename}', [
-            'as'   => 'thumb.attachment',
-            'uses' => 'FileController@getThumbnailFromAttachment',
-        ]);
+        Route::get('{attachment}/{filename}', [
+            'as'   => 'file.attachment',
+            'uses' => 'FileController@getImageFromAttachment',
+        ])->where(['attachment' => '[0-9]+',]);
 
         Route::get('thumb/{hash}/{filename}', [
             'as'   => 'thumb.hash',
             'uses' => 'FileController@getThumbnailFromHash',
         ])->where(['hash' => "[a-f0-9]{32}",]);
+
+        Route::get('thumb/{attachment}/{filename}', [
+            'as'   => 'thumb.attachment',
+            'uses' => 'FileController@getThumbnailFromAttachment',
+        ])->where(['attachment' => '[0-9]+',]);
     });
 } else {
     Route::group(['prefix' => '{board}/file',], function() {
@@ -49,7 +49,7 @@ if (false && !is_hidden_service() && config('app.url_media', false))
         Route::get('{attachment}/{filename}', [
             'as'   => 'file.attachment',
             'uses' => 'FileController@getImageFromAttachment',
-        ]);
+        ])->where(['attachment' => '[0-9]+',]);
 
         Route::get('thumb/{hash}/{filename}', [
             'as'   => 'thumb.hash',
